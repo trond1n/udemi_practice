7bf49f797d7e74ccf343028a1c727ad5d4c6502a
Yuri Trondin <lopata@2ch.hk>
2020-08-31 22:15:21 +0500

    закончил со второй практической задачей

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 "use strict";
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,12 +7,28 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
 
-console.log(personalMovieDB);
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("просмотрено слишком мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('вы классический зритель');
+} else if(personalMovieDB.count>=30){
+    console.log('Вы киноман')
+} else{
+    console.log('Произошла ошибка')
+}
+
+
+    console.log(personalMovieDB);
